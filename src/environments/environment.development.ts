0514cc4b4d01edd63e9ee149788488f8bf4e9ea1
Jorge Rossel <rossel.jorge.jr@gmail.com>
2025-06-05 19:48:10 -0400

    Docker configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
     OPEN_AI_API_KEY:
-    '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',
+    '{api-key}',
     BACKEND_URL: 'http://127.0.0.1:9191/api/graphql'
 
 };
+
+//Localhost
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   OPEN_AI_API_KEY:
-    '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',
-  BACKEND_URL: 'http://3.12.97.167:9191/api/graphql'
+    '{api-key}',
+  BACKEND_URL: 'http://127.0.0.1:9191/api/graphql'
 };
+
+//Nube
